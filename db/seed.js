9d2f8fa05ab81f3ed95726cd67d40f063be1e008
Paris Taylor <paristechprofile@gmail.com>
2019-03-13 08:39:35 -0700

    completes seed logic and is ready for testing

--- db/seed.js
+++ db/seed.js
@@ -3,19 +3,30 @@ const
     course = require('./course'),
     student = require('./student')
 
-db.Course.deleteMany({},(err,courses)=>{
+db.Student.deleteMany({}, (err, students)=>{
     if (err) {throw err};
-    console.log(courses)
-    db.Course.create(course, (err, courses)=>{
-        if(err) {throw err};
-        console.log(courses)
-
-        db.Student.deleteMany({}, (err, students)=>{
+    console.log(`removed all`, students)
+    db.Student.create(student, (err, students)=>{
+        if (err) {throw err};
+        console.log(`adding`, students)
+        db.Course.deleteMany({},(err,courses)=>{
             if (err) {throw err};
-            console.log(students)
-            db.Student.create(student, (err, students)=>{
-                if (err) {throw err};
-                console.log(students)
+            console.log(`removed all`, courses)
+            course.forEach(course => {
+                let newCourse = new db.Course({
+                    name: course.name,
+                    description: course.description,
+                    location: course.location,
+                    startDate: course.startDate
+                })
+                db.Student.findOne({name: student.name}, (err, foundStudent)=>{
+                    if (err) {throw err};
+                    newCourse.student = foundStudent;
+                    newCourse.save((err, savedCourse) =>{
+                        if (err) {throw err};
+                        console.log(`enrolled ${savedCourse.student} into ${savedCourse.name}`)
+                    });
+                });       
             });
         });
     });
