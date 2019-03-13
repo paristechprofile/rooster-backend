592591fdc6509a40aa8e573b3200488b3d56d9db
Paris Taylor <paristechprofile@gmail.com>
2019-03-12 19:58:50 -0700

    adds course and student controllers

--- controllers/course.js
+++ controllers/course.js
@@ -3,9 +3,15 @@ const
 
 module.exports = {
     findAll: (req,res)=>{
-        
+        db.Course.find({}, (err, foundCourses) =>{
+            if(err) {
+                console.log('error retriving courses', err)
+            } res.json(foundCourses)
+        });
     },
     findById: (req,res)=>{
-        
+        db.Course.find({_id: req.params.id}, (error,cities)=>{
+            res.json(cities);
+        });
     }
 };
--- controllers/student.js
+++ controllers/student.js
@@ -3,18 +3,45 @@ const
 
 module.exports = {
     findAll: (req,res)=>{
-        
+        db.Student.find({}, (err, foundStudents) =>{
+            if(err) {
+                console.log('error retriving students', err)
+            } res.json(foundStudents);
+        });
     },
     findById: (req,res)=>{
-        
+        db.Student.find({_id: req.params.id}, (err,foundStudent)=>{
+            if (err){
+                console.log('error retrieving student', err)
+            } res.json(foundStudent);
+        });
     },
     addStudent: (req,res)=>{
-
+        let newStudent = new db.Student({
+            name: req.body.name,
+            age: req.body.age,
+            bio: req.body.bio,
+            DOB: req.body.DOB
+        });
+        newStudent.save((err, addedStudent) =>{
+            if (err) {throw err}; //note: changing err to save time
+            res.json(addedStudent)
+        }); 
     },
     editStudent: (req,res)=>{
-
+        const studentId = req.params.id;
+        db.Student.findOneAndUpdate({_id: studentId},req.body,{new:true},
+            (err, modifiedStudent) => {
+                if(err) {throw err}
+                res.json(modifiedStudent);
+            } 
+        );
     },
     deleteStudent:(req,res)=>{
-
+        const studentId = req.params.id;
+        db.Student.findOneAndDelete({_id: studentId}, (err, deletedStudent)=>{
+            if(err) {throw err}
+            res.json(deletedStudent);
+        })
     }
 };
