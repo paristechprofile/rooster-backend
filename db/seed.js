a07191bd4d7d59f7402adba8b0cf36db18e4089a
Paris Taylor <paristechprofile@gmail.com>
2019-03-12 20:11:36 -0700

    adds delete and add functions for seeded database

--- db/seed.js
+++ db/seed.js
@@ -2,3 +2,18 @@ const
     db = require('../models'),
     course = require('./course'),
     student = require('./student')
+
+db.Course.deleteMany({},(err,courses)=>{
+    db.Course.create(course, (err, courses)=>{
+        if(err) {throw err};
+
+        db.Student.deleteMany({}, (err, students)=>{
+            if (err) {throw err};
+
+            db.Student.create(data, (err, students)=>{
+                if (err) {throw err};
+
+            });
+        });
+    });
+});
