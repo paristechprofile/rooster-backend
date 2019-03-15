4c19d6f53741f756eac7037146e55d7363f8475a
Paris Taylor <paristechprofile@gmail.com>
2019-03-15 07:18:33 -0700

    fixes seeding issue. backend complete

--- db/seed.js
+++ db/seed.js
@@ -4,15 +4,16 @@ const
     student = require('./student')
 
 db.Student.deleteMany({}, (err, students)=>{
+    console.log(students)
     if (err) {throw err};
-    // console.log(`removed all`, students)
     db.Student.create(student, (err, students)=>{
+        console.log(students)
         if (err) {throw err};
-        // console.log(`adding`, students)
         db.Course.deleteMany({},(err,courses)=>{
+            console.log(courses)
             if (err) {throw err};
-            // console.log(`removed all`, courses)
             course.forEach(course => {
+                console.log(course)
                 let newCourse = new db.Course({
                     name: course.name,
                     description: course.description,
@@ -20,13 +21,12 @@ db.Student.deleteMany({}, (err, students)=>{
                     startDate: course.startDate
                 })
                 db.Student.find({}, (err, foundStudent)=>{
-                    // console.log(foundStudent)
+                    console.log(foundStudent)
                     if (err) {throw err};
                     newCourse.students = foundStudent;
                     newCourse.save((err, savedCourse) =>{
-                        if (err) {throw err};
                         console.log(savedCourse)
-                        // console.log(`enrolled ${savedCourse.student} into ${savedCourse.name}`)
+                        if (err) {throw err};
                     });
                 });       
             });
